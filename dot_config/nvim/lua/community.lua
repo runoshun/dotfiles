8c50e62be47ac8ae55bb861c33e3e9d4ef01539d
runoshun <runoshun@gmail.com>
2024-09-21 21:01:26 +0900

    Update .bashrc Update .config/mise/config.toml Update .config/nvim/lua/community.lua Update .tmux.conf Update 0_init.sh

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -5,18 +5,24 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+
   -- colorscheme
-  --{ import = "astrocommunity.colorscheme.nordic-nvim" },
-  --{ import = "astrocommunity.colorscheme.sonokai" },
+  -- { import = "astrocommunity.colorscheme.nordic-nvim" },
+  -- { import = "astrocommunity.colorscheme.sonokai" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  --{ import = "astrocommunity.colorscheme.kanagawa-paper-nvim" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  --{ import = "astrocommunity.colorscheme.miasma-nvim" },
-  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  -- { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  -- { import = "astrocommunity.colorscheme.kanagawa-paper-nvim" },
+  -- { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  -- { import = "astrocommunity.colorscheme.miasma-nvim" },
+  -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
 
+  -- packs
   { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.rust" },
 
-  { import = "astrocommunity.code-runner.molten-nvim" },
+  -- misc
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
+  { import = "astrocommunity.docker.lazydocker" },
 }
