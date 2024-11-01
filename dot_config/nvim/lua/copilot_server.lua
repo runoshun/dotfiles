96e4808ecf8fe7ca920f4f34e790e553f8e51e20
runoshun <runoshun@gmail.com>
2024-11-02 06:09:04 +0900

    style: Format code with consistent indentation in copilot_server.lua

--- dot_config/nvim/lua/copilot_server.lua
+++ dot_config/nvim/lua/copilot_server.lua
@@ -1,30 +1,29 @@
-
 local M = {}
 
 local job_id = nil
 
 function M.start()
-  if job_id == nil then
-    job_id = vim.fn.jobstart('deno run -A dot_config/nvim/scripts/github-copilot-proxy/main.ts', { detach = true })
-    print("Copilot Proxy server started")
-  else
-    print("Copilot Proxy server is already running")
-  }
+	if job_id == nil then
+		job_id = vim.fn.jobstart("deno run -A dot_config/nvim/scripts/github-copilot-proxy/main.ts", { detach = true })
+		print("Copilot Proxy server started")
+	else
+		print("Copilot Proxy server is already running")
+	end
 end
 
 function M.stop()
-  if job_id ~= nil then
-    vim.fn.jobstop(job_id)
-    job_id = nil
-    print("Copilot Proxy server stopped")
-  else
-    print("Copilot Proxy server is not running")
-  end
+	if job_id ~= nil then
+		vim.fn.jobstop(job_id)
+		job_id = nil
+		print("Copilot Proxy server stopped")
+	else
+		print("Copilot Proxy server is not running")
+	end
 end
 
 function M.restart()
-  M.stop()
-  M.start()
+	M.stop()
+	M.start()
 end
 
 return M
