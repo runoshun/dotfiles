661bb9e70858906f4011aa18c4f50a31e26552e6
Shunsuke Fukuda <shunsuke_fukuda@pkshatech.com>
2025-03-04 05:42:24 +0000

    Update .chezmoiexternal.toml Update .config/mise/config.toml Update .config/nvim/lua/plugins/edit/leap-nvim.lua

--- dot_config/nvim/lua/plugins/edit/leap-nvim.lua
+++ dot_config/nvim/lua/plugins/edit/leap-nvim.lua
@@ -3,7 +3,11 @@ return {
 		"ggandor/leap.nvim",
 		lazy = false,
 		config = function()
-			require("leap").create_default_mappings()
+			require("leap")
+			vim.keymap.set({ "n", "x" }, "s", "<Plug>(leap)")
+			vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+			vim.keymap.set("o", "s", "<Plug>(leap-forward)")
+			vim.keymap.set("o", "S", "<Plug>(leap-backward)")
 		end,
 	},
 }
