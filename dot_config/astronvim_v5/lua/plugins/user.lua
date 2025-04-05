60f231f5b89463f3c0d2b4af527ebe42d4bf28b9
runoshun <runoshun@gmail.com>
2025-04-05 21:16:39 +0900

    Add .config/astronvim_v5/config/mcp-hub.json Add .config/astronvim_v5/lua/plugins/avante.lua Update .config/astronvim_v5/lua/plugins/user.lua

--- dot_config/astronvim_v5/lua/plugins/user.lua
+++ dot_config/astronvim_v5/lua/plugins/user.lua
@@ -191,30 +191,30 @@ return {
 		},
 		keys = {
 			{ "<leader>ai", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
-			{
-				"<leader>as",
-				"<cmd>AiderTerminalSend<cr>",
-				desc = "Send to Aider",
-				mode = { "n", "v" },
-			},
-			{ "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
-			{ "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-			{ "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-			{ "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
-			{ "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-			-- Example nvim-tree.lua integration if needed
-			{
-				"<leader>a+",
-				"<cmd>AiderTreeAddFile<cr>",
-				desc = "Add File from Tree to Aider",
-				ft = "NvimTree",
-			},
-			{
-				"<leader>a-",
-				"<cmd>AiderTreeDropFile<cr>",
-				desc = "Drop File from Tree from Aider",
-				ft = "NvimTree",
-			},
+			-- {
+			-- 	"<leader>as",
+			-- 	"<cmd>AiderTerminalSend<cr>",
+			-- 	desc = "Send to Aider",
+			-- 	mode = { "n", "v" },
+			-- },
+			-- { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
+			-- { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
+			-- { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+			-- { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+			-- { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
+			-- -- Example nvim-tree.lua integration if needed
+			-- {
+			-- 	"<leader>a+",
+			-- 	"<cmd>AiderTreeAddFile<cr>",
+			-- 	desc = "Add File from Tree to Aider",
+			-- 	ft = "NvimTree",
+			-- },
+			-- {
+			-- 	"<leader>a-",
+			-- 	"<cmd>AiderTreeDropFile<cr>",
+			-- 	desc = "Drop File from Tree from Aider",
+			-- 	ft = "NvimTree",
+			-- },
 		},
 		dependencies = {
 			"folke/snacks.nvim",
